c811984ab19464c76a08966e9b0d47c10dad05b0
DESKTOP-HK8J74I\riyenas0925 <riyenas0925@gmail.com>
2017-10-09 20:44:38 +0900

    처음

---
+++ MyApplication/app/src/main/java/org/riyenas/interface_check/SuccessActivity.java
@@ -0,0 +1,12 @@
+package org.riyenas.interface_check;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SuccessActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
