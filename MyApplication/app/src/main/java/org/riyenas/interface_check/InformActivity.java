e798f84c48c589327d233b66176cd4d9a914059c
riyenas0925 <riyenas0925@gmail.com>
2017-11-01 20:25:26 +0900

    명부, 공지사항 디자인 적용

--- MyApplication/app/src/main/java/org/riyenas/interface_check/InformActivity.java
+++ MyApplication/app/src/main/java/org/riyenas/interface_check/InformActivity.java
@@ -1,6 +1,7 @@
 package org.riyenas.interface_check;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
